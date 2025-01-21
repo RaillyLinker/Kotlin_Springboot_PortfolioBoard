08889ba2b937068a01582d1f1d51e944965fa6c4
RaillyLinker <raillylinker@gmail.com>
2025-01-21 14:25:36 +0900

    feat : init project

---
+++ src/test/kotlin/com/raillylinker/kotlin_springboot_portfolio_board/KotlinSpringbootPortfolioBoardApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raillylinker.kotlin_springboot_portfolio_board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootPortfolioBoardApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
